5c837a9ac26b75e47cf5fdf77d9b138eb181b2f6
Glowstudent <55334764+Glowstudent777@users.noreply.github.com>
2021-06-29 15:27:38 -0500

    Add files via upload

--- js/console.js
+++ js/console.js
@@ -2,3 +2,5 @@ console.warn(
     '%cHello Traveller \nUnless You Know What You Are Doing \nDO NOT TOUCH ANYTHING',
     'font-weight:bold; font-size:20pt; font-family:"Comic Sans MS"; color:white;'
 );
+
+console.timeLog(this);
